07ecc9d6ca8ad3e0a39818fe9ca3e74d2a5fc7a9
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-09-30 09:03:58 +0100

    feat:add animation to error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
+
 const Error = () => {
+  const navigate = useNavigate();
   return (
     <section className="flex h-[80dvh] gap-4 flex-col justify-center items-center text-center w-full">
-      <h1 className="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-9xl font-extrabold text-transparent">
+      <h1 className="bg-linear-to-r from-pink-500 to-violet-500 bg-clip-text text-9xl font-extrabold text-transparent animate-bounce">
         Oops!
       </h1>
       <h3 className="font-bold text-xl">404- PAGE NOT FOUND</h3>
@@ -9,7 +12,9 @@ const Error = () => {
         The page you are looking for might have been removed,had its name changed or is temporarily unavailable
       </p>
 
-      <button className="w-70 rounded-3xl text-white bg-indigo-700 py-2">GO TO HOMEPAGE</button>
+      <button className="w-70 rounded-3xl text-white bg-indigo-700 py-2" onClick={() => navigate('/')}>
+        GO TO HOMEPAGE
+      </button>
     </section>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <main>
+      <section>
+        <h1>Discover Your Next Great Read</h1>
+        <p>
+          Explore thousands of books, read reviews, and find your perfect literary companion in our curated collection.
+          Browse Books
+        </p>
+        <button>Browse Books</button>
+      </section>
+    </main>
+  );
+};
+export default Home;
